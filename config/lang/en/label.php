d074b4921bc7c52581ae353d2480e7e44baa3e5f
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-09-24 14:13:12 +0700

    penambahan localization untuk en

---
+++ config/lang/en/label.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'home'   => 'Home',
+    'create' => 'Create',
+    'edit'   => 'Edit',
+    'delete' => 'Delete',
+    'back'   => 'Back',
+    'view'   => 'View',
+    'confirm_msg' => 'are you sure ?'
+];
