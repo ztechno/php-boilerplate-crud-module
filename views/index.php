d01e3a03c88f3c593268495c2f975b87c469b313
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-11-12 15:35:02 +0700

    update allowed route

--- views/index.php
+++ views/index.php
@@ -9,9 +9,11 @@ table td img {
         <p class="h4 m-0"><?php get_title() ?></p>
         <div class="right-button ms-auto">
             <?= $crudRepository->additionalButtonBeforeCreate() ?>
+            <?php if(is_allowed(parsePath(routeTo('crud/create', ['table'=>$tableName])), auth()->id)): ?>
             <a href="<?= crudRoute('crud/create', $tableName) ?>" class="btn btn-success btn-sm">
                 <i class="fa-solid fa-plus"></i> <?= __('crud.label.create') ?>
             </a>
+            <?php endif ?>
             <?= $crudRepository->additionalButtonAfterCreate() ?>
         </div>
     </div>
