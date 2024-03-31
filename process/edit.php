ead355243d4691411bba0b23893c6fec2238a200
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-03-31 20:13:47 +0700

    update textarea tinymce

--- process/edit.php
+++ process/edit.php
@@ -56,7 +56,7 @@ Page::pushHead('<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist
 Page::pushHead('<script src="https://cdn.tiny.cloud/1/rsb9a1wqmvtlmij61ssaqj3ttq18xdwmyt7jg23sg1ion6kn/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>');
 Page::pushHead("<script>
 tinymce.init({
-  selector: 'textarea',
+    selector: 'textarea:not(.select2-search__field)',
   plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
   toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
 });
