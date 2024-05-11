e8604a82871915fa6b6fc42d9e40e0a9392a89a9
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-05-11 15:51:52 +0700

    update guard

--- guards/index.php
+++ guards/index.php
@@ -16,6 +16,11 @@ if($auth && $route == 'auth/login')
     die();
 }
 
+$authRoute = [
+    'auth/login',
+    'auth/logout',
+];
+
 if(!in_array($route, $authRoute) && $auth && !is_allowed($route, $auth->id))
 {
     die('Error 403. Unauthorized');
